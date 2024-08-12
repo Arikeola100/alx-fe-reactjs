33578999c89f180f5ac407de8a3f0bee9f326a4f
Blessing Ajayi <arikeolaa100@gmail.com>
2024-08-12 01:07:56 +0100

    Rearranged the components

--- alx-react-app/src/App.jsx
+++ alx-react-app/src/App.jsx
@@ -1,16 +1,15 @@
 import './App.css'
-import UserProfile from "./components/UserProfile"
 import WelcomeMessage from "./components/WelcomeMessage"
 import Header from "./components/Header"
 import MainContent from "./components/MainContent"
 import Footer from "./components/Footer"
+import UserProfile from "./components/UserProfile"
+
 function App() {
 
   return (
     <>
-     <div>
-     <UserProfile name = "Blessing" age = "25" Bio = "loves reading and outdoor camping." />
-     </div>
+      
       <div>
       <WelcomeMessage />
       </div>
@@ -22,6 +21,9 @@ function App() {
       </div>
       <div>
       <Footer />
+      </div>
+      <div>
+      <UserProfile name = "Blessing Ajayi" age = "25" Bio = "loves reading and outdoor camping." />
       </div>
     </>
   )
