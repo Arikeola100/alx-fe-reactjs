157e5be4c16aad420aff2113a6500d7bee628003
Blessing Ajayi <arikeolaa100@gmail.com>
2024-08-18 23:33:32 +0100

    refactoring task

---
+++ alx-react-app-props/src/App.jsx
@@ -0,0 +1,16 @@
+import './App.css';
+import React from 'react';
+import UserContext from './components/UserContext.js';
+import ProfilePage from './components/ProfilePage'
+
+function App() {
+  const userData = { name: "Jane Doe", email: "jane.doe@example.com" };
+
+  return (
+    <UserContext.Provider value={userData}>
+      <ProfilePage />
+    </UserContext.Provider>
+  );
+}
+
+export default App;
