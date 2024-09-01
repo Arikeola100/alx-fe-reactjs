e6b9caabb5ac4d5311ca8a9a42c5a8a301607a7f
Blessing Ajayi <arikeolaa100@gmail.com>
2024-09-01 20:44:19 +0100

    added setRecipes fuction

--- recipe-sharing-app/src/components/recipeStore.js
+++ recipe-sharing-app/src/components/recipeStore.js
@@ -4,6 +4,7 @@ const useRecipeStore = create((set) => ({
   recipes: [],
   addRecipe: (newRecipe) =>
     set((state) => ({ recipes: [...state.recipes, newRecipe] })),
+    setRecipes: (recipes) => set({ recipes }),
   deleteRecipe: (recipeId) =>
     set((state) => ({
       recipes: state.recipes.filter((recipe) => recipe.id !== recipeId),
