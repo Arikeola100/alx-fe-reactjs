313ec362053cd25d3c3518b513c7499e575ccaae
Blessing Ajayi <arikeolaa100@gmail.com>
2024-09-15 04:26:27 +0100

    added homepage

--- recipe-sharing-platform/src/App.jsx
+++ recipe-sharing-platform/src/App.jsx
@@ -1,11 +1,11 @@
-
+import HomePage from './components/HomePage'
 import './App.css'
 
 function App() {
   return (
-    <div className="min-h-screen bg-blue-500 flex justify-center items-center">
-      <h1 className="text-white text-4xl font-bold">Hello, Tailwind CSS!</h1>
-    </div>
+    <>
+    <HomePage/>
+    </> 
   );
 }
 
