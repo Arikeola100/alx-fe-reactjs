6ba40939a3e2c7ec690d35be3b94d9f612b26e2a
Blessing Ajayi <arikeolaa100@gmail.com>
2024-09-22 23:02:23 +0100

    removed '

--- github-user-search/src/components/Search.jsx
+++ github-user-search/src/components/Search.jsx
@@ -42,7 +42,7 @@ const Search = () => {
 
       {/* Conditional Rendering */}
       {loading && <p>Loading...</p>}
-      {error && <p>Looks like we can't find the user.</p>}
+      {error && <p>Looks like we cant find the user.</p>}
       {userData && (
         <div className="mt-4 bg-white shadow-lg rounded p-4">
           <img src={userData.avatar_url} alt={userData.login} className="w-20 h-20 rounded-full" />
